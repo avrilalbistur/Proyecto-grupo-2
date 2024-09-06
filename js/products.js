45df732ce0ba0ce1f4e76901fe8271c3605e3dcb
Avril Albistur <aalbistur27@gmail.com>
2024-09-06 14:38:04 -0300

    commit

--- js/products.js
+++ js/products.js
@@ -1,4 +1,4 @@
-// Function to create product boxes
+// Función para crear las cajas de los productos
 let showProducts = (productsArray) =>{
     let htmlProductsToAppend = "";
     productsArray.forEach(product => {
@@ -16,15 +16,18 @@ let showProducts = (productsArray) =>{
     })
     document.getElementById('product-container').innerHTML = htmlProductsToAppend;
 }
+// Función del buscador (Desafiate entrega 3)
+let searchBar = document.getElementById('search-bar');
+let productContainer = document.getElementById('product-container')
 
-//function that will be executed when the document completely is fully loaded 
+//Función que se va a ejecutar cuando el contenido HTML esté cargado
 
 document.addEventListener('DOMContentLoaded',(e)=>{
    getJSONData(PRODUCTS_URL+101+EXT_TYPE)
         .then(object =>{
             if(object.status === 'ok'){
               let productsArray = object.data.products;
-            showProducts(productsArray);  
+              showProducts(productsArray); 
             };
         } );
 });
