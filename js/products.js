2fc42ae46e23f078f800ee791718ff8a0aedb8ba
solcasamara <solcasamara1@gmail.com>
2024-09-12 15:33:36 -0300

    commit

--- js/products.js
+++ js/products.js
@@ -1,7 +1,46 @@
+const ORDER_ASC_BY_PRICE = "0";
+const ORDER_DESC_BY_PRICE = "9";
+const ORDER_BY_PROD_COUNT = "Cant.";
 let minPrice = undefined;
 let maxPrice = undefined;
 let productsArray = []
+let currentSortCriteria = undefined;
+let currentProductsArray = [];
 
+
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.productCount);
+            let bCount = parseInt(b.productCount);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_ASC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            let aPrice = parseInt(a.cost);
+            let bPrice = parseInt(b.cost);
+
+            if ( aPrice > bPrice ){ return -1; }
+            if ( aPrice < bPrice ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            let aPrice = parseInt(a.cost);
+            let bPrice = parseInt(b.cost);
+
+            if ( aPrice < aPrice ){ return -1; }
+            if ( aPrice > bPrice ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
 
 // Function to create product boxes
 let showProducts = (productsArray) =>{
@@ -26,6 +65,18 @@ let showProducts = (productsArray) =>{
     document.getElementById('product-container').innerHTML = htmlProductsToAppend;
 }
 
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    showProducts(currentProductsArray);
+}
+
 //function that will be executed when the document completely is fully loaded 
 
 document.addEventListener('DOMContentLoaded', (e) => {
@@ -44,7 +95,17 @@ document.addEventListener('DOMContentLoaded', (e) => {
     }
 });
 
+document.getElementById("sortPriceAsc").addEventListener("click", function(){
+    sortAndShowProducts(ORDER_ASC_BY_PRICE,productsArray);
+});
 
+document.getElementById("sortPriceDesc").addEventListener("click", function(){
+    sortAndShowProducts(ORDER_DESC_BY_PRICE,productsArray);
+});
+
+document.getElementById("sortByCount").addEventListener("click", function(){
+    sortAndShowProducts(ORDER_BY_PROD_COUNT, productsArray);
+});
 
 document.getElementById("clearRangeFilter").addEventListener("click", function(){
     document.getElementById("rangeFilterPriceMin").value = "";
@@ -78,5 +139,3 @@ document.getElementById("rangeFilterPrice").addEventListener("click", function()
 
     showProducts(productsArray);
 });
-
-
